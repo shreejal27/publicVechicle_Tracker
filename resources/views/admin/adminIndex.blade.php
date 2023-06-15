23fbc65a23dd5a0f5f813360cc02d1bc501aa866
Shreejal Maharjan <shreejal27@gmail.com>
2023-06-15 17:16:52 +0545

    adjusted footer at the bottom despite the size of content

--- resources/views/admin/adminIndex.blade.php
+++ resources/views/admin/adminIndex.blade.php
@@ -8,5 +8,29 @@
             I have all the powers bitch
 
         </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. A, itaque? Cupiditate autem pariatur rem dolorem. Et,
+            consectetur provident aspernatur beatae eaque delectus reiciendis expedita magni, reprehenderit ipsum error quae
+            natus.
+        </p>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore laudantium adipisci error rerum quisquam,
+            cupiditate rem distinctio doloremque. Amet in tempore nihil fuga quibusdam tempora quaerat eaque ea! Eaque,
+            ipsam?</h1>
+        <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus autem iste eveniet quibusdam, nesciunt
+            deserunt accusamus quia quod velit sequi consequuntur unde provident earum eligendi eum exercitationem tempore
+            sint ipsa?</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A, itaque? Cupiditate autem pariatur rem dolorem. Et,
+                consectetur provident aspernatur beatae eaque delectus reiciendis expedita magni, reprehenderit ipsum error quae
+                natus.
+            </p>
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore laudantium adipisci error rerum quisquam,
+                cupiditate rem distinctio doloremque. Amet in tempore nihil fuga quibusdam tempora quaerat eaque ea! Eaque,
+                ipsam?</h1>
+            <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus autem iste eveniet quibusdam, nesciunt
+                deserunt accusamus quia quod velit sequi consequuntur unde provident earum eligendi eum exercitationem tempore
+                sint ipsa?</h1>
+          
+     
     </section>
 @endsection
