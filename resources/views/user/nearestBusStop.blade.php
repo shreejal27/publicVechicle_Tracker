99d1f2c48d6775292dc224494e1dcb6031edd6a9
Shreejal Maharjan <shreejal27@gmail.com>
2023-06-19 20:51:02 +0545

    showed user's location without having to press the button

--- resources/views/user/nearestBusStop.blade.php
+++ resources/views/user/nearestBusStop.blade.php
@@ -4,22 +4,23 @@
         width: 100%;
     }
 </style>
+
 @extends('necessary.user_template')
+
 @section('content')
-    <h1>This is user nearest bus stop</h1>
+    <h1>This is the nearest bus stop</h1>
     <section class="maps">
-        <h1 class="center"> Available Bus Stops</h1>
+        <h1 class="center">Available Bus Stops</h1>
         <br>
         <div id="map"></div>
         <br>
-        <p>Click the button to get your coordinates.</p>
-        <button onclick="getLocation()">Try It</button>
         <p id="demo"></p>
     </section>
 
     <script>
         var x = document.getElementById("demo");
         var lat, lng;
+        var map;
 
         function initMap() {
             var options = {
@@ -31,65 +32,15 @@
             };
 
             // Create a new map instance
-            var map = new google.maps.Map(document.getElementById("map"), options);
+            map = new google.maps.Map(document.getElementById("map"), options);
 
             // Add markers to the map
             const userIcon = {
                 url: '{{ asset('images/markerIcons/userMarker.png') }}',
                 scaledSize: new google.maps.Size(32, 32) // Set the desired size of the icon
             };
-            const userMarker = new google.maps.Marker({
-                position: {
-                    lat: lat,
-                    lng: lng
-                },
-                map,
-                title: "You are here",
-                icon: userIcon
 
-            });
-            const userInfoWindow = new google.maps.InfoWindow({
-                content: "You are here"
-            });
-
-            userMarker.addListener("click", () => {
-                userInfoWindow.open(map, userMarker);
-            });
-
-            // Add markers for bus stops
-            @foreach ($stops as $stop)
-                {
-                    const icon = {
-                        @if ($stop->vehicle_type == 'bus')
-                            url: '{{ asset('images/markerIcons/B.png') }}',
-                        @elseif ($stop->vehicle_type == 'micro')
-                            url: '{{ asset('images/markerIcons/M.png') }}',
-                        @elseif ($stop->vehicle_type == 'tempo')
-                            url: '{{ asset('images/markerIcons/T.png') }}',
-                        @endif
-                        scaledSize: new google.maps.Size(32, 32) // Set the desired size of the icon
-                    };
-                    const marker = new google.maps.Marker({
-                        position: {
-                            lat: {{ $stop->latitude }},
-                            lng: {{ $stop->longitude }}
-                        },
-                        map,
-                        title: "{{ $stop->info }}",
-                        icon: icon
-                    });
-                    const infoWindow = new google.maps.InfoWindow({
-                        content: "{{ $stop->info }}",
-                    });
-
-                    marker.addListener("click", () => {
-                        infoWindow.open(map, marker);
-                    });
-                }
-            @endforeach
-        }
-
-        function getLocation() {
+            // Get the user's current location
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(showPosition, showError, {
                     enableHighAccuracy: true
@@ -97,37 +48,93 @@
             } else {
                 x.innerHTML = "Geolocation is not supported by this browser.";
             }
-        }
 
-        function showPosition(position) {
-            // Update the latitude and longitude variables
-            lat = position.coords.latitude;
-            lng = position.coords.longitude;
+            function showPosition(position) {
+                // Update the latitude and longitude variables
+                lat = position.coords.latitude;
+                lng = position.coords.longitude;
 
-            console.log(lat, lng);
+                // Create a marker for the user's location
+                const userMarker = new google.maps.Marker({
+                    position: {
+                        lat: lat,
+                        lng: lng
+                    },
+                    map,
+                    title: "You are here",
+                    icon: userIcon
+                });
 
+                const userInfoWindow = new google.maps.InfoWindow({
+                    content: "You are here"
+                });
 
-            // Call the function to initialize the map
-            initMap();
-        }
+                userMarker.addListener("click", () => {
+                    userInfoWindow.open(map, userMarker);
+                });
 
-        function showError(error) {
-            switch (error.code) {
-                case error.PERMISSION_DENIED:
-                    x.innerHTML = "User denied the request for Geolocation.";
-                    break;
-                case error.POSITION_UNAVAILABLE:
-                    x.innerHTML = "Location information is unavailable.";
-                    break;
-                case error.TIMEOUT:
-                    x.innerHTML = "The request to get user location timed out.";
-                    break;
-                case error.UNKNOWN_ERROR:
-                    x.innerHTML = "An unknown error occurred.";
-                    break;
+                // Call the function to add markers for bus stops
+                addBusStopMarkers();
+            }
+
+            function showError(error) {
+                switch (error.code) {
+                    case error.PERMISSION_DENIED:
+                        x.innerHTML = "User denied the request for Geolocation.";
+                        break;
+                    case error.POSITION_UNAVAILABLE:
+                        x.innerHTML = "Location information is unavailable.";
+                        break;
+                    case error.TIMEOUT:
+                        x.innerHTML = "The request to get user location timed out.";
+                        break;
+                    case error.UNKNOWN_ERROR:
+                        x.innerHTML = "An unknown error occurred.";
+                        break;
+                }
+            }
+
+            function addBusStopMarkers() {
+                // Add markers for bus stops
+                @foreach ($stops as $stop)
+                    {
+                        const icon = {
+                            @if ($stop->vehicle_type == 'bus')
+                                url: '{{ asset('images/markerIcons/B.png') }}',
+                            @elseif ($stop->vehicle_type == 'micro')
+                                url: '{{ asset('images/markerIcons/M.png') }}',
+                            @elseif ($stop->vehicle_type == 'tempo')
+                                url: '{{ asset('images/markerIcons/T.png') }}',
+                            @endif
+                            scaledSize: new google.maps.Size(32, 32) // Set the desired size of the icon
+                        };
+                        const marker = new google.maps.Marker({
+                            position: {
+                                lat: {{ $stop->latitude }},
+                                lng: {{ $stop->longitude }}
+                            },
+                            map,
+                            title: "{{ $stop->info }}",
+                            icon: icon
+                        });
+                        const infoWindow = new google.maps.InfoWindow({
+                            content: "{{ $stop->info }}",
+                        });
+
+                        marker.addListener("click", () => {
+                            infoWindow.open(map, marker);
+                        });
+                    }
+                @endforeach
             }
         }
+
+        // Call the function to initialize the map
+        initMap();
     </script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD3U0k0wYHv6RGnT6_JYOMxMTJVfa8vL48&callback=initMap" async
         defer></script>
 @endsection
+
+
+{{-- AIzaSyD3U0k0wYHv6RGnT6_JYOMxMTJVfa8vL48 --}}
