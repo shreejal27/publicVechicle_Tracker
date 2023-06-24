10f25f68df15d61fa9140a0c82719d99cf2e940f
Shreejal Maharjan <shreejal27@gmail.com>
2023-06-24 21:31:22 +0545

    adjusted height of slider Images

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,6 +21,11 @@
         clear: both;
     }
 
+    .sliderImage {
+        height: 650px;
+        width: 100%;
+    }
+
     .blogImages {
         width: 250px;
         margin-bottom: 1rem;
@@ -56,13 +61,16 @@
             </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img class="d-block w-100" src="{{ asset('images/sliderImages/sliderImage1.jpg') }}" alt="First slide">
+                    <img class="d-block w-100 sliderImage" src="{{ asset('images/sliderImages/sliderImage1.jpg') }}"
+                        alt="First slide">
                 </div>
                 <div class="carousel-item">
-                    <img class="d-block w-100" src="{{ asset('images/sliderImages/sliderImage2.jpg') }}" alt="Second slide">
+                    <img class="d-block w-100 sliderImage" src="{{ asset('images/sliderImages/sliderImage2.jpg') }}"
+                        alt="Second slide">
                 </div>
                 <div class="carousel-item">
-                    <img class="d-block w-100" src="{{ asset('images/sliderImages/sliderImage3.jpg') }}" alt="Third slide">
+                    <img class="d-block w-100 sliderImage" src="{{ asset('images/sliderImages/sliderImage3.jpg') }}"
+                        alt="Third slide">
                 </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
