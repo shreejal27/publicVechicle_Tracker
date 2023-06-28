524725ec9ea2567465e51cf67bf486e969ce5cf5
Shreejal Maharjan <shreejal27@gmail.com>
2023-06-28 22:27:27 +0545

    tried to design for fare page in admin

--- resources/views/admin/farePrice.blade.php
+++ resources/views/admin/farePrice.blade.php
@@ -4,6 +4,13 @@
         <h1>This is farePrice</h1>
     </section>
     <section>
-        <p>Hello</p>
+        <p>Add new Fare and Distance</p>
     </section>
+    <form action="" method="post">
+        <label>Distance (in kms): </label>
+        <input type="text" name="distance" id="distance"> <br>
+        <label>Price: </label>
+        <input type="text" name="distance" id="distance"> <br>
+        <input type="submit" value="Add">
+    </form>
 @endsection
