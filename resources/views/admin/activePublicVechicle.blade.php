a38bead8f357053e3410516113516182da0a5496
Shreejal Maharjan <shreejal27@gmail.com>
2023-06-30 00:51:46 +0545

    used dijkastra algorithm and highlighted the shortest path

--- resources/views/admin/activePublicVechicle.blade.php
+++ resources/views/admin/activePublicVechicle.blade.php
@@ -1,6 +1,6 @@
 @extends('necessary.admin_template')
 @section('content')
-    <h1>This is to track the live public vechicle</h1>
+    <h1>This is to track the live public vehicle</h1>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/leaflet@1.7.1/dist/leaflet.css" />
     <style>
         #map {
@@ -20,7 +20,7 @@
                 attribution: '&copy; OpenStreetMap contributors'
             }).addTo(map);
 
-            var userMarker, placeMarker1, placeMarker2;
+            var userMarker, placeMarkers = [];
 
             // Get the user's current location
             if ("geolocation" in navigator) {
@@ -33,27 +33,85 @@
 
                     // Update the map view to the user's location
                     map.setView([latitude, longitude], 13);
+
+                    // Define the coordinates for the other places
+                    var places = [{
+                            name: 'Place 1',
+                            latitude: 27.6982600,
+                            longitude: 85.2993750
+                        },
+                        {
+                            name: 'Place 2',
+                            latitude: 27.6847670,
+                            longitude: 85.2993170
+                        },
+                        {
+                            name: 'Place 3',
+                            latitude: 27.680864,
+                            longitude: 85.296528
+                        }
+                    ];
+
+                    // Create markers for the other places with custom icons
+                    var placeIcon = L.icon({
+                        iconUrl: 'path/to/place-icon.png',
+                        iconSize: [32, 32],
+                        iconAnchor: [16, 32],
+                        popupAnchor: [0, -32]
+                    });
+
+                    for (var i = 0; i < places.length; i++) {
+                        var place = places[i];
+                        var marker = L.marker([place.latitude, place.longitude], {
+                            icon: placeIcon
+                        }).addTo(map);
+                        placeMarkers.push(marker);
+
+                        // Calculate the distance between the user's location and the place
+                        var distance = calculateDistance(latitude, longitude, place.latitude, place.longitude);
+
+                        // Add the distance as a property of the marker for later use
+                        marker.distance = distance;
+                    }
+
+                    // Sort the place markers based on distance in ascending order
+                    placeMarkers.sort(function(a, b) {
+                        return a.distance - b.distance;
+                    });
+
+                    // Get the marker with the shortest distance
+                    var shortestDistanceMarker = placeMarkers[0];
+
+                    // Highlight the shortest path on the map
+                    var pathCoordinates = [
+                        [latitude, longitude],
+                        [shortestDistanceMarker.getLatLng().lat, shortestDistanceMarker.getLatLng().lng]
+                    ];
+
+                    L.polyline(pathCoordinates, {
+                        color: 'blue'
+                    }).addTo(map);
                 });
             }
 
-            // Define the coordinates for the other places
-            var place1 = [27.6982600, 85.2993750];
-            var place2 = [27.6847670, 85.2993170];
+            // Function to calculate the distance between two sets of latitude and longitude coordinates
+            function calculateDistance(lat1, lon1, lat2, lon2) {
+                var R = 6371; // Radius of the earth in kilometers
+                var dLat = deg2rad(lat2 - lat1);
+                var dLon = deg2rad(lon2 - lon1);
+                var a =
+                    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+                    Math.sin(dLon / 2) * Math.sin(dLon / 2);
+                var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+                var distance = R * c; // Distance in kilometers
+                return distance;
+            }
 
-            // Create markers for the other places with custom icons
-            var placeIcon = L.icon({
-                iconUrl: '/images/markerIcons/B.png',
-                iconSize: [32, 32],
-                iconAnchor: [16, 32],
-                popupAnchor: [0, -32]
-            });
-
-            placeMarker1 = L.marker(place1, {
-                icon: placeIcon
-            }).addTo(map);
-            placeMarker2 = L.marker(place2, {
-                icon: placeIcon
-            }).addTo(map);
+            // Function to convert degrees to radians
+            function deg2rad(deg) {
+                return deg * (Math.PI / 180);
+            }
         </script>
     </section>
 @endsection
