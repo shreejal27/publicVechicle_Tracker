e29312451f843e42e705249e5f478ed0e6de7359
Shreejal Maharjan <shreejal27@gmail.com>
2023-07-10 23:13:51 +0545

    changed contact Us page css

--- resources/views/contactUs.blade.php
+++ resources/views/contactUs.blade.php
@@ -1,11 +1,56 @@
 @extends('necessary.common_template')
 
 @section('content')
-    <section class="title">
-        <h2>How Can We Help You?</h2>
+    <style>
+        .info {
+            background-color: #A9907E;
+            color: black;
+        }
+
+        .form {
+            background-color: #675D50;
+        }
+
+        input,
+        select {
+            height: 40px;
+            background-color: #A9907E;
+            color: #F3DEBA;
+            border-radius: 10px;
+            border: 1px solid #F3DEBA;
+        }
+
+        select {
+            padding-left: 15px;
+        }
+
+        ::placeholder {
+            padding-left: 15px;
+            color: #F3DEBA;
+
+        }
+
+        textarea {
+            border-radius: 10px;
+            color: white;
+        }
+
+        input[type=submit] {
+            width: 80px;
+            height: 40px;
+            background-color: #F3DEBA;
+            color: black;
+        }
+
+        input[type=submit]:hover {
+            background-color: #ABC4AA;
+        }
+    </style>
+    <section class="title mt-4 mb-4">
+        <h2 class="text-center">How Can We Help You?</h2>
     </section>
     <section class="main">
-        <div class="info" style="width: 47.5%; border: 1px solid black; padding:1rem; float: left">
+        <div class="info" style="width: 48%; border: 1px solid black; padding:1rem; float: left">
             <p>
                 Hotline Number:
                 <br>
@@ -30,7 +75,7 @@
                 info.publicvechicletracker@gmail.com
             </p>
         </div>
-        <div class="form ml-3" style="width: 47.5%; border: 1px solid red; padding:1rem; float:left;">
+        <div class="form ml-3" style="width: 48%; border: 1px solid red; padding:1rem; float:left;">
             <form action="">
                 @csrf
                 <input type="text" placeholder="Full Name" style="width: 48%" required>
@@ -38,7 +83,7 @@
                 <br>
                 <br>
                 <input type="number" placeholder="Number" style="width: 48%;" required>
-                <select name="" id="" style="width: 48%; height:27px" required>
+                <select name="" id="" style="width: 48%; " required>
                     <option value="">Subject Type</option>
                     <option value="complain">Complain</option>
                     <option value="feedback">Feedback</option>
@@ -50,7 +95,7 @@
                 <input type="text" placeholder="Subject" style="width: 100%;" required>
                 <br>
                 <br>
-                <textarea style="width: 100%; height: 15rem;" required></textarea>
+                <textarea style="width: 100%; height: 15rem; background-color:#A9907E" required></textarea>
                 <br>
                 <br>
                 <input type="submit" value="Send">
