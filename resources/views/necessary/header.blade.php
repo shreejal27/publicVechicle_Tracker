822fd3d0e2f7387727012e27447ba8934a40a480
Shreejal Maharjan <shreejal27@gmail.com>
2023-07-30 21:53:19 +0545

    added favicon.ico => project icon

--- resources/views/necessary/header.blade.php
+++ resources/views/necessary/header.blade.php
@@ -17,6 +17,7 @@
 
 <head>
     <meta charset="UTF-8">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Public Vehicle Tracker</title>
@@ -24,4 +25,6 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
+
+
 </head>
