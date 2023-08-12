4d1bbdd6867e52f2a2a81de33c661388e0c073d1
Shreejal Maharjan <shreejal27@gmail.com>
2023-08-12 22:17:10 +0545

    changed the feedback form in the user side

--- resources/views/user/feedbackComplain.blade.php
+++ resources/views/user/feedbackComplain.blade.php
@@ -1,131 +1,4 @@
-<style>
-    .formBox {
-        background-color: #15172b;
-        border-radius: 20px;
-        box-sizing: border-box;
-        height: auto;
-        padding: 20px;
-        width: 50%;
-        text-align: center;
-        margin-left: 25%;
-    }
-
-    .title {
-        color: #eee;
-        font-family: sans-serif;
-        font-size: 36px;
-        font-weight: 600;
-
-    }
-
-    .subtitle {
-        color: #eee;
-        font-family: sans-serif;
-        font-size: 16px;
-        font-weight: 600;
-        margin-top: 10px;
-    }
-
-    .input-container {
-        height: 50px;
-        position: relative;
-        width: 100%;
-    }
-
-    .ic1 {
-        margin-top: 40px;
-    }
-
-    .ic2 {
-        margin-top: 30px;
-    }
-
-    .input {
-        background-color: #303245;
-        border-radius: 12px;
-        border: 0;
-        box-sizing: border-box;
-        color: #eee;
-        font-size: 18px;
-        height: 100%;
-        outline: 0;
-        padding: 4px 20px 0;
-        width: 100%;
-    }
-
-    .cut {
-        background-color: #15172b;
-        border-radius: 10px;
-        height: 20px;
-        left: 20px;
-        position: absolute;
-        top: -20px;
-        transform: translateY(0);
-        transition: transform 200ms;
-        width: 76px;
-    }
-
-    .cut-short {
-        width: 50px;
-    }
-
-    .iLabel {
-        color: #65657b;
-        font-family: sans-serif;
-        left: 20px;
-        line-height: 14px;
-        pointer-events: none;
-        position: absolute;
-        transform-origin: 0 50%;
-        transition: transform 200ms, color 200ms;
-        top: 20px;
-    }
-
-    .input:focus~.cut {
-        transform: translateY(8px);
-    }
-
-    .input:focus~.iLabel {
-        transform: translateY(-30px) translateX(10px) scale(0.75);
-    }
-
-    .input:not(:focus)~.iLabel {
-        color: #808097;
-    }
-
-    .input:focus~.iLabel {
-        color: #dc2f55;
-    }
-
-    .submit {
-        background-color: #08d;
-        border-radius: 12px;
-        border: 0;
-        box-sizing: border-box;
-        color: #eee;
-        cursor: pointer;
-        font-size: 18px;
-        height: 50px;
-        margin-top: 38px;
-        text-align: center;
-        width: 100%;
-    }
-
-    .submit:active {
-        background-color: #06b;
-    }
-
-    textarea {
-        height: 17rem;
-        width: 100%;
-    }
-
-    select {
-        height: 30px;
-        float: left;
-        width: 25%;
-    }
-</style>
+<link rel="stylesheet" type="text/css" href="{{ asset('formStyles.css') }}">
 @extends('necessary.user_template')
 @section('content')
     @if (session('message'))
@@ -133,38 +6,33 @@
             {{ session('message') }}
         </div>
     @endif
-    <div class="formBox">
-        <div class="title">We are here to assist you</div>
-        <div class="subtitle">Please complete the form below!</div>
-        <form action="{{ route('storeComplainFeedback') }}" method="POST">
-            @csrf
-            <div class="input-container ic1">
-                <input placeholder="" type="text" class="input" id="username" name="username" required>
-                <div class="cut"></div>
-                <label class="iLabel" for="username">UserName</label>
-            </div>
+    <div class="center-container mt-5">
+        <div class="form-container col-md-4 mb-3">
+            <p class="title">We are here to assist you</p>
+            <p class="subtitle">Please complete the form below</p>
+            <form class="form" id="userCredentialForm" action="{{ route('storeComplainFeedback') }}" method="POST">
+                @csrf
+                <div class="input-group">
+                    <label>UserName:</label>
+                    <input type="text" name="username" required>
+                </div>
+                <div class="input-group">
+                    <label>Select Issue:</label>
+                    <select name="formType" required>
+                        <option value="complain">Complain</option>
+                        <option value="feedback">Feedback</option>
+                        <option value="query">Query</option>
+                        <option value="request">Request</option>
+                    </select>
 
-            {{-- <div class="input-container ic2">
-                <input placeholder="" type="text" class="input" id="phone">
-                <div class="cut cut-short"></div>
-                <label class="iLabel" for="phone">Phone</label>
-            </div> --}}
-            <br>
-            <select name="formType" required>
-                <option value="complain">Complain</option>
-                <option value="feedback">Feedback</option>
-                <option value="query">Query</option>
-                <option value="request">Request</option>
-            </select>
-            <br>
-            <br>
-            <textarea name="formDescription" placeholder="Write your message here" required></textarea>
-            {{-- <div class="input-container ic2">
-                <input placeholder="" type="text" class="input" id="email">
-                <div class="cut cut-short"></div>
-                <label class="iLabel" for="email">Email</label>
-            </div> --}}
-            <input type="submit" value="submit" class="Submit">
-        </form>
+                </div>
+                <div class="input-group">
+                    <label>Message:</label>
+                </div>
+                <textarea name="formDescription" placeholder="Write your message here" rows="5"></textarea>
+
+                <button class="sign mt-3" type="submit">Submit</button>
+            </form>
+        </div>
     </div>
 @endsection
