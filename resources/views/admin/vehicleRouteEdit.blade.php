1329dd832c459de839a9dae6f33df19fff365ef1
Shreejal Maharjan <shreejal27@gmail.com>
2023-08-21 20:39:28 +0545

    changed the form css of edit vehicle route

--- resources/views/admin/vehicleRouteEdit.blade.php
+++ resources/views/admin/vehicleRouteEdit.blade.php
@@ -1,16 +1,25 @@
+<link rel="stylesheet" type="text/css" href="{{ asset('formStyles.css') }}">
 @extends('necessary.admin_template')
 @section('content')
-    <section class="title">
-        <h1>Edit Vehicle Route</h1>
-    </section>
     <section>
-        <form action="{{ route('updateVehicleRoute', $vehicleDetails->id) }}" method="post">
-            @csrf
-            <label>Vehicle Name:</label>
-            <input type="text" name="vehicle_name" value="{{ $vehicleDetails->vehicle_name }}"> <br>
-            <label>Vehicle Routes:</label>
-            <input type="text" name="vehicle_routes" value="{{ $vehicleDetails->vehicle_routes }}"> <br>
-            <input type="submit" value="Update">
-        </form>
+        <div class="center-container mt-5">
+            <div class="form-container col-md-4 mb-3">
+                <p class="title">Edit Vehicle Route</p>
+                {{-- <p class="subtitle">Add new Fare and Distance</p> --}}
+                <form class="form" id="userCredentialForm" action="{{ route('updateVehicleRoute', $vehicleDetails->id) }}"
+                    method="POST">
+                    @csrf
+                    <div class="input-group">
+                        <label>Vehicle Name:</label>
+                        <input type="text" name="vehicle_name" value="{{ $vehicleDetails->vehicle_name }}">
+                    </div>
+                    <div class="input-group">
+                        <label>Price:</label>
+                        <input type="text" name="vehicle_routes" value="{{ $vehicleDetails->vehicle_routes }}">
+                    </div>
+                    <button class="sign mt-3" type="submit">Update</button>
+                </form>
+            </div>
+        </div>
     </section>
 @endsection
