117a87374be2bb14b6239f9f76a7938b0d1917db
Shreejal Maharjan <shreejal27@gmail.com>
2023-08-27 20:55:15 +0545

    added a bootstrap table to show limited data

--- resources/views/admin/viewFeedbackComplain.blade.php
+++ resources/views/admin/viewFeedbackComplain.blade.php
@@ -1,9 +1,17 @@
 <link rel="stylesheet" type="text/css" href="{{ asset('tableStyles.css') }}">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap4.min.css">
+
+<style>
+    table.dataTable {
+        border-collapse: collapse !important;
+    }
+</style>
 @extends('necessary.admin_template')
 
 @section('content')
     <section class="m-3 mt-4">
-        <table class="table table-hover col-md-12 col-sm-12 col-lg-12 col-xl-12">
+        <table class="table table-hover col-md-12 col-sm-12 col-lg-12 col-xl-12" id="complainFeedback">
             <thead>
                 <th colspan="8"> All Requests</th>
                 <tr>
@@ -33,4 +41,13 @@
             </tbody>
         </table>
     </section>
+    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap4.min.js"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#complainFeedback').DataTable();
+        });
+    </script>
 @endsection
