12c599f210cd10980756550ecef1b811c37a52c8
Shreejal Maharjan <shreejal27@gmail.com>
2023-09-06 17:21:19 +0545

    showed complainfeedback in sweetalert when clicked on a specific row

--- resources/views/admin/viewFeedbackComplain.blade.php
+++ resources/views/admin/viewFeedbackComplain.blade.php
@@ -6,6 +6,10 @@
     table.dataTable {
         border-collapse: collapse !important;
     }
+
+    td {
+        cursor: pointer;
+    }
 </style>
 @extends('necessary.admin_template')
 
@@ -18,24 +22,25 @@
                     {{-- <th>SN</th> --}}
                     <th>Date</th>
                     <th>Name</th>
-                    <th>Email</th>
+                    {{-- <th>Email</th> --}}
                     <th>Number </th>
                     <th>Type</th>
                     <th>Subject</th>
-                    <th>Description</th>
+                    {{-- <th>Description</th> --}}
                 </tr>
             </thead>
             <tbody>
                 @foreach ($complainFeedbacks as $complainFeedback)
-                    <tr>
+                    <tr data-description="{{ $complainFeedback['description'] }}"
+                        data-email="{{ $complainFeedback['email'] }}">
                         {{-- <td>{{ $loop->iteration }}</td> --}}
                         <td>{{ \Carbon\Carbon::parse($complainFeedback['created_at'])->format('Y-m-d (l)') }}</td>
                         <td>{{ $complainFeedback['fullname'] }}</td>
-                        <td>{{ $complainFeedback['email'] }}</td>
+                        {{-- <td>{{ $complainFeedback['email'] }}</td> --}}
                         <td>{{ $complainFeedback['number'] }}</td>
                         <td>{{ $complainFeedback['type'] }}</td>
                         <td>{{ $complainFeedback['subject'] }}</td>
-                        <td>{{ $complainFeedback['description'] }}</td>
+                        {{-- <td>{{ $complainFeedback['description'] }}</td> --}}
                     </tr>
                 @endforeach
             </tbody>
@@ -47,12 +52,38 @@
 
     <script type="text/javascript">
         $(document).ready(function() {
-            $('#complainFeedback').DataTable({
+            var table = $('#complainFeedback').DataTable({
                 "order": [
                     [0, "desc"]
                 ],
                 "lengthMenu": [5, 10, 25, 50], // Customize the number of entries options
                 "pageLength": 10 // Default number of entries shown on page load
+            });
+
+            //for click event listener to table rows
+            $('#complainFeedback tbody').on('click', 'tr', function() {
+
+                var rowData = table.row(this).data(); // to get data of specific row
+                var description = $(this).data('description');
+                var email = $(this).data('email');
+
+                // SweetAlert dialog
+                var content = '<div style="text-align: left; font-size: 1rem;">' +
+                    '<br><strong>Date:</strong> ' + rowData[0] + '<br><br>' +
+                    '<strong>Name:</strong> ' + rowData[1] + '<br><br>' +
+                    '<strong>Email:</strong> ' + email + '<br><br>' +
+                    '<strong>Number:</strong> ' + rowData[2] + '<br><br>' +
+                    '<strong>Type:</strong> ' + rowData[3] + '<br><br>' +
+                    '<strong>Subject:</strong> ' + rowData[4] + '<br><br>' +
+                    '<strong>Description:</strong><br><br> ' + description +
+                    '</div>';
+
+                // Display the SweetAlert dialog with the row data
+                Swal.fire({
+                    title: 'Information',
+                    html: content,
+                    
+                });
             });
         });
     </script>
