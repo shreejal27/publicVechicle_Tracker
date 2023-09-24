2a847cc621162e26a2ddc46f59ed147840aabeaa
Shreejal Maharjan <shreejal27@gmail.com>
2023-09-24 16:15:37 +0545

    removed driver table and added driver card in admin

--- resources/views/admin/viewDriversReports.blade.php
+++ resources/views/admin/viewDriversReports.blade.php
@@ -3,14 +3,76 @@
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap4.min.css">
 
 <style>
-    table.dataTable {
+    /* table.dataTable {
         border-collapse: collapse !important;
+    } */
+    .right {
+        display: flex;
+        justify-content: flex-end;
+        margin-bottom: 1rem;
+    }
+
+    .right input {
+        width: 15rem;
+        height: 2rem;
+        border: 1px solid #ccc;
+        padding: 0 0.6rem;
+        box-sizing: border-box;
+        background-color: #675D50;
+        color: #F3DEBA;
+    }
+
+    .right input::placeholder {
+        color: #F3DEBA;
+    }
+
+    .box {
+        max-width: 1200px;
+        /* Adjust the max width as needed */
+        margin: 0 auto;
+    }
+
+    .grid-container {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(25%, 1fr));
+        /* Create columns with equal width */
+        gap: 20px;
+        /* Adjust the gap between elements as needed */
+    }
+
+
+    .driverProfile {
+        height: 250px;
+        background-color: #675D50;
+        color: #F3DEBA;
+        text-align: center;
+        margin-bottom: 20px;
+        /* Adjust margin as needed */
+        box-sizing: border-box;
+        border: 1px solid #ccc;
+        /* Add border or styling as needed */
+        padding: 10px;
+    }
+
+    .image-container {
+        text-align: center;
+    }
+
+    #profileImage {
+        width: 5rem;
+        height: 5rem;
+        border-radius: 8rem;
+        margin: 0 auto;
+        margin-top: 1rem;
+        margin-bottom: 1rem;
+        object-fit: cover;
+        border: 2px solid #F3DEBA;
     }
 </style>
 @extends('necessary.admin_template')
 @section('content')
     <section class="m-3 mt-4">
-        <table class="table table-hover col-md-12 col-sm-12 col-lg-12 col-xl-12" id="driverTable">
+        {{-- <table class="table table-hover col-md-12 col-sm-12 col-lg-12 col-xl-12" id="driverTable">
             <thead>
                 <th colspan="8"> All Drivers</th>
                 <tr>
@@ -46,7 +108,26 @@
                 @endforeach
 
             </tbody>
-        </table>
+        </table> --}}
+        <div class="right">
+            <input type="text" id="profileSearch" placeholder="Search">
+        </div>
+        <div class="box ">
+            <div class="grid-container">
+                @foreach ($drivers as $driver)
+                    <div class="driverProfile ">
+                        <div class="image-container">
+                            <img src="{{ asset('images/drivers/' . ($driver['profileImage'] ? $driver['profileImage'] : 'anonymous.jpg')) }}"
+                                alt="Profile Image" id="profileImage">
+                        </div>
+                        <h4>{{ $driver['firstname'] }} {{ $driver['lastname'] }}</h4>
+                        <h5>{{ $driver['vehicle_type'] }}</h5>
+                        <h6><i class="fa-solid fa-location-dot"></i> {{ $driver['address'] }}</h6>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+
     </section>
     <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
@@ -62,6 +143,34 @@
                     "targets": [6],
                     "type": "dom-status"
                 }]
+            });
+        });
+    </script>
+
+    <script>
+        const profileSearchInput = document.getElementById('profileSearch');
+        const gridContainer = document.querySelector('.grid-container');
+
+        // Add an event listener to the search input
+        profileSearchInput.addEventListener('input', function() {
+            const searchTerm = profileSearchInput.value.toLowerCase();
+
+            // Loop through each driver profile and check if it matches the search term
+            gridContainer.querySelectorAll('.driverProfile').forEach(function(profile) {
+                const profileName = profile.querySelector('h4').textContent.toLowerCase();
+                const profileVehicle = profile.querySelector('h5').textContent.toLowerCase();
+                const profileAddress = profile.querySelector('h6').textContent.toLowerCase();
+
+                // Show or hide the profile based on the search term
+                if (
+                    profileName.includes(searchTerm) ||
+                    profileVehicle.includes(searchTerm) ||
+                    profileAddress.includes(searchTerm)
+                ) {
+                    profile.style.display = 'block';
+                } else {
+                    profile.style.display = 'none';
+                }
             });
         });
     </script>
