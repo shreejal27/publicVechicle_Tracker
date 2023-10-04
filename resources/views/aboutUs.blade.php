c089322cfa143bc08ec0211e25bd261c37fb4dcd
Shreejal Maharjan <shreejal27@gmail.com>
2023-10-04 23:48:30 +0545

    changed the content in about us page

--- resources/views/aboutUs.blade.php
+++ resources/views/aboutUs.blade.php
@@ -31,20 +31,21 @@
                     <section>
                         <h2> Who We Are</h2>
                         <p>
-                            Established in 1962, Sajha Yatayat is a cooperative company providing public transportation
-                            services. Currently, we are operating a fleet of 71 large Euro 3 and 4 standard diesel buses.
-                            Most of our buses operate primarily in the Kathmandu valley, servicing around 26,000 passengers
-                            daily across the metropolitan region. We also provide long-route transportation services to
-                            districts neighbouring the valley, including Baglung district, Butwal and Bhairahawa in
-                            Rupandehi district, as well as Kushma in Parbat district. <br><br>
+                            Pioneering the future of public transportation, the "Public Vehicle Tracker" project is
+                            committed to revolutionizing the way people navigate urban transit systems. Established with a
+                            vision for smarter and more efficient public mobility, our project is at the forefront of
+                            modernizing public transportation services. <br><br>
 
-                            Our fleet covers over 74,000 km daily and connects two metropolitan cities, namely Kathmandu and
-                            Lalitpur. We also cover seven municipalities which are Budhanilkantha, Madhya Thimi, Bhaktapur,
-                            Dakshinkali, Karyabinayak, Mahalaxmi, and Godawari.<br><br>
+                            Our initiative embodies a blend of cutting-edge technology and a deep understanding of the
+                            evolving needs of commuters. Through real-time tracking, data analytics, and user-centric
+                            applications, we aim to provide passengers with a seamless and convenient travel experience.
+                            Whether you're a daily commuter or an occasional traveler, our platform is designed to empower
+                            you with information, making your journey more predictable and hassle-free.<br><br>
 
-                            We aim to provide clean transportation services. With this aim in mind, we are adding 40 Battery
-                            Operated Electric Buses (BEB) to our current fleet and plan to replace all our diesel buses with
-                            BEB by 2025.
+                            With a commitment to excellence and a passion for enhancing urban mobility, the "Public Vehicle
+                            Tracker" project is not just about tracking vehicles; it's about transforming the way people
+                            move within their cities. Join us on this journey toward smarter, more sustainable, and more
+                            accessible public transportation. Together, we're shaping the future of urban mobility.
                         </p>
                     </section>
                 </div>
@@ -56,25 +57,42 @@
 
                         <h5>The Beginning</h5>
                         <p>
-                            With the main objective of operating cheap, accessible and reliable transport service for the
-                            general public, Sajha Yatayat was registered as a cooperative under the then Cooperatives Act
-                            1959 AD on Wednesday, 16th March 1962. Four months after its establishment, the organisation
-                            started providing public transport services in the urban area of Kathmandu on 16th July 1962, Mr
-                            Vishwabandhu Thapa was the founding chairman of this organisation. Ministries and departments of
-                            the Government of Nepal, Kathmandu Metropolitan City, Lalitpur Metropolitan City, Godavari
-                            Municipality, Mahalakshmi Municipality, local level, various organisations and the general
-                            public have also invested in our organisation.
+                            In the annals of modern urban transit, the inception of the "Public Vehicle Tracker" project
+                            marks a significant milestone. With an unwavering commitment to providing affordable,
+                            accessible, and reliable transportation services for the public, our project's journey commenced
+                            under the banner of innovation and cooperative spirit.<br><br>
+
+                            On the historic date of Wednesday, 16th March 1962, Sajha Yatayat was officially registered as a
+                            cooperative entity under the then Cooperatives Act of 1959 AD. Just four months after its formal
+                            establishment, this visionary organization embarked on its mission to revolutionize public
+                            transportation services. On 16th July 1962, the first Sajha Yatayat buses hit the urban streets
+                            of Kathmandu, introducing a new era of mobility. Mr. Vishwabandhu Thapa, a visionary leader,
+                            served as the founding chairman of this remarkable initiative.<br><br>
+
+                            Over the years, our cooperative has garnered investments from various stakeholders, including
+                            government ministries and departments, Kathmandu and Lalitpur Metropolitan Cities, Godavari and
+                            Mahalakshmi Municipalities, local communities, organizations, and the general public. This
+                            collective effort has played a pivotal role in shaping our organization's legacy.
                         </p>
                         <h5>Current Scenario</h5>
                         <p>
-                            Currently, Sajha Yatayat operates 71 large size buses in its fleet out of which four buses are
-                            running in long routes and the remaining 67 are operating as city buses within Kathmandu Valley.
-                            Adding to this, 40 mid-sized electric buses have been procured which will be in operation by
-                            August, 2022.<br><br>
+                            The "Public Vehicle Tracker" project has evolved into a dynamic and indispensable component of
+                            modern urban transportation systems. As of [Current Date], the project is at the forefront of
+                            revolutionizing how the public commutes, offering real-time tracking and information services
+                            for public vehicles.<br><br>
 
-                            There are 21 employees of different posts and levels working under the management of Sajha
-                            Yatayat. The manpower required for the operation of the bus has been outsourced to 124 bus
-                            drivers, 124 conductors, 3 security guards and 1 light driver.
+                            Our project is proud to report a significant expansion in terms of both coverage and impact.
+                            Currently, the system is actively tracking and monitoring a substantial fleet of public
+                            vehicles, serving bustling metropolitan areas, suburban regions, and even connecting neighboring
+                            districts. This wide-ranging coverage ensures that commuters across various geographies can
+                            access accurate and up-to-date information about public vehicle locations, routes, and
+                            schedules.<br><br>
+
+                            In terms of technological advancements, the "Public Vehicle Tracker" project remains at the
+                            cutting edge. Continuous enhancements and updates have been implemented to provide an intuitive
+                            and user-friendly experience for commuters. The project's mobile application and web-based
+                            platforms have garnered a growing user base, signifying the increased reliance on this
+                            technology for daily commuting needs.
                         </p>
                     </section>
                 </div>
@@ -84,16 +102,13 @@
                     <section>
                         <h2> Vision & Strategy</h2>
                         <p>
-                            Our Vision statement is as follows:
-                        </p>
+                            The "Public Vehicle Tracker" project envisions a future where public transportation is seamlessly accessible, highly efficient, and environmentally sustainable. At its core, the project aims to redefine urban mobility by harnessing the power of technology to empower commuters and enhance the overall public transportation experience.<br><br>
 
-                        <h5> "To be the leading transport agency providing affordable, efficient and safe mode of urban and inter-district public transportation services in Nepal, as well as flag-carrier cross border service to regional cities."</h5>
-                        <p>
-                            To achieve our vision, we embark on certain short-, medium- and long-term strategy. 
-                        </p>
-                        <h5> Our short-term strategy</h5>
-                        <p>
-                            Revive its operations in Kathmandu Valley's urban space, with the aim of generating public goodwill as well as visibility, while maximizing the sustainability factor before the organization enters into a more profitably restructured entity in the medium- and long-term.
+                            Our vision is to become the go-to platform for commuters across [Your Project's Geographic Region] seeking reliable, real-time information about public vehicles. We aspire to bridge the gap between commuters and public transportation by providing a user-friendly, comprehensive, and intuitive solution that simplifies the daily commute.<br><br>
+                            
+                            To achieve this vision, our strategy is multifaceted. First and foremost, we are committed to continuous innovation and technological advancement. We will invest in cutting-edge tracking and communication technologies to ensure that commuters can access accurate and up-to-date information about public vehicle locations, routes, and schedules. This will not only reduce waiting times but also contribute to a more efficient use of public transportation resources.<br><br>
+                            
+                            Furthermore, we are dedicated to fostering partnerships and collaborations with public transportation agencies, local governments, and other stakeholders. By working together, we can integrate our tracking and monitoring system seamlessly into existing transportation infrastructure, ensuring a cohesive and interconnected urban mobility network.
                         </p>
 
                     </section>
